d3567c50ad5da773c425e9183a68f8651685272b
Douglas Squirrel <ds@douglassquirrel.com>
2011-11-25 21:36:51 +0000

    Add run_tests process

---
+++ run_tests.py
@@ -0,0 +1,19 @@
+# Copyright Douglas Squirrel 2011
+# This program comes with ABSOLUTELY NO WARRANTY. 
+# It is free software, and you are welcome to redistribute it under certain conditions; see the GPLv3 license in the file LICENSE for details.
+
+import messageboard
+
+def run_tests(verb):
+    #try:
+        print "Running tests for %s" % verb
+        messageboard.post(verb='run_tests.%s' % verb, noun='')
+        print "Sent run_tests message"
+
+    #except StandardError as e:
+    #    print "Got exception %s" % str(e)
+
+def run_tests_tests():
+    return True
+
+messageboard.start_consuming(verb='process_started', callback=run_tests, run_tests=run_tests_tests)
---
+++ run_tests_test.py
@@ -0,0 +1,19 @@
+# Copyright Douglas Squirrel 2011
+# This program comes with ABSOLUTELY NO WARRANTY. 
+# It is free software, and you are welcome to redistribute it under certain conditions; see the GPLv3 license in the file LICENSE for details.
+
+import messageboard, unittest
+
+class TestRunTests(unittest.TestCase):
+    def tearDown(self):
+        messageboard.post('stop.__go')
+
+    def test_runs_tests_and_reports_success(self):
+        (channel, queue_name) = messageboard.bind('run_tests.__go')
+        messageboard.post(verb='process_started', noun='__go')
+        (method, body) = messageboard.get_one_message(channel, queue_name, 'run_tests.__go')
+        self.assertNotEqual(None, method)
+
+if __name__ == '__main__':
+    unittest.main()
+
