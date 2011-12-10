8d2a3fc1dd19f06deffc0c29e181cc8456966887
Douglas Squirrel <ds@douglassquirrel.com>
2011-12-10 23:03:51 +0000

    add collector test

--- core/collector_test.py
+++ core/collector_test.py
@@ -4,7 +4,19 @@
 
 import json, messageboard
 
-def collect_one_message(connection):
+def collect_no_message():
+    connection = messageboard.get_connection()
+    messageboard.bind(connection=connection, key='ready_to_collect.c0m_test_messages_received')
+    messageboard.post(connection, 'collect', json.dumps({'messages': ['c0m_1'], 'response': 'c0m_test_messages_received'}))
+    (method, body) = messageboard.get_one_message(connection)
+    if None == method:
+        return False    
+
+    messageboard.bind(connection=connection, key='c0m_test_messages_received')
+    (key, body) = messageboard.get_one_message(connection)
+    return (None == key)
+
+def collect_one_message():
     connection = messageboard.get_connection()
     messageboard.bind(connection=connection, key='ready_to_collect.c1m_test_messages_received')
     messageboard.post(connection, 'collect', json.dumps({'messages': ['c1m_1'], 'response': 'c1m_test_messages_received'}))
@@ -17,8 +29,13 @@ def collect_one_message(connection):
     (key, body) = messageboard.get_one_message(connection)
     return ('c1m_test_messages_received' == key)
 
+def collect_two_messages():
+    return True
+
 def collector_test(connection, key, body):
-    result = collect_one_message(connection)
+    result =   collect_no_message() \
+           and collect_one_message() \
+           and collect_two_messages()
     messageboard.post(connection, 'collector_test_result', json.dumps(result))
 
 connection = messageboard.get_connection()
