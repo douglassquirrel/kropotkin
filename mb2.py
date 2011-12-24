b52c28b22a434ac60c566296bce5687161976382
Douglas Squirrel <ds@douglassquirrel.com>
2011-12-24 10:10:20 +0000

    get one message in mb2

--- mb2.py
+++ mb2.py
@@ -14,5 +14,17 @@ class MessageBoard:
             body=json.dumps({'key': key})
         else:
             body=json.dumps({'key': key, 'content': content})
-
         self.channel.basic_publish(exchange='kropotkin', routing_key='mb.post', body=body)
+
+    def watch_for(self, key):
+        queue = self.channel.queue_declare(exclusive=True).method.queue
+        self.channel.queue_bind(exchange='kropotkin', queue=queue, routing_key=key)
+        return queue
+
+    def get_one_message(self, queue):
+        method, properties, body = self.channel.basic_get(queue=queue, no_ack=True)
+        if method.NAME != 'Basic.GetEmpty':
+            return (method.routing_key, json.loads(body))
+        else:
+            return (None, None)
+
--- mb2_test.py
+++ mb2_test.py
@@ -5,6 +5,34 @@
 import json, mb2, pika, time, unittest
 
 class TestMessageBoard(unittest.TestCase):
+    def setUp(self):
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+        self.channel = connection.channel()
+        self.channel.exchange_declare(exchange='kropotkin', type='topic')
+        self.queue_name = self.channel.queue_declare(exclusive=True).method.queue
+        self.mb = mb2.MessageBoard()
+
+    def test_post_without_content(self):
+        self.channel.queue_bind(exchange='kropotkin', queue=self.queue_name, routing_key='mb.post')        
+        self._post_and_check_without_content(key='_test_key') 
+        self._post_and_check_without_content(key='_another_test_key') 
+
+    def test_post_with_content(self):
+        self.channel.queue_bind(exchange='kropotkin', queue=self.queue_name, routing_key='mb.post')        
+        self._post_and_check_with_content(key='_test_key', content='_test_content') 
+        self._post_and_check_with_content(key='_another_test_key', content='_another_test_content') 
+
+    def test_watch_for_and_get_one_message(self):
+        mb = mb2.MessageBoard()
+        self._watch_for_send_and_check(key='_test_key', content={'datum': '_test_datum'})
+        self._watch_for_send_and_check(key='another_test_key', content={'datum': 'another_test_datum'})
+
+    def test_returns_none_if_no_message(self):
+        queue = self.mb.watch_for(key='_test_key') 
+        received_key, received_content = self.mb.get_one_message(queue)
+        self.assertEqual(None, received_key)
+        self.assertEqual(None, received_content)
+
     def _wait_for_message_and_check(self, expected_key, expected_body, seconds_to_wait=1):
         for i in range(seconds_to_wait * 100):
             method, properties, body = self.channel.basic_get(queue=self.queue_name, no_ack=True)
@@ -15,30 +43,20 @@ class TestMessageBoard(unittest.TestCase):
             time.sleep(0.01)
         self.fail('Did not get expected message key=%s, body=%s' % (expected_key, expected_body))
 
-    def _post_and_check_without_content(self, mb, key):
-        mb.post(key=key)
+    def _post_and_check_without_content(self, key):
+        self.mb.post(key=key)
         self._wait_for_message_and_check(expected_key='mb.post', expected_body=json.dumps({'key': key}))
 
-    def _post_and_check_with_content(self, mb, key, content):
-        mb.post(key=key, content=content)
+    def _post_and_check_with_content(self, key, content):
+        self.mb.post(key=key, content=content)
         self._wait_for_message_and_check(expected_key='mb.post', expected_body=json.dumps({'key': key, 'content': content}))
 
-    def setUp(self):
-        connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-        self.channel = connection.channel()
-        self.channel.exchange_declare(exchange='kropotkin', type='topic')
-        self.queue_name = self.channel.queue_declare(exclusive=True).method.queue
-
-    def test_post_without_content(self):
-        self.channel.queue_bind(exchange='kropotkin', queue=self.queue_name, routing_key='mb.post')        
-        mb = mb2.MessageBoard()
-        self._post_and_check_without_content(mb, key='_test_key') 
-        self._post_and_check_without_content(mb, key='_another_test_key') 
-
-    def test_post_with_content(self):
-        self.channel.queue_bind(exchange='kropotkin', queue=self.queue_name, routing_key='mb.post')        
-        mb = mb2.MessageBoard()
-        self._post_and_check_with_content(mb, key='_test_key', content='_test_content') 
+    def _watch_for_send_and_check(self, key, content):
+        queue = self.mb.watch_for(key=key) 
+        self.channel.basic_publish(exchange='kropotkin', routing_key=key, body=json.dumps(content))
+        received_key, received_content = self.mb.get_one_message(queue)
+        self.assertEqual(key, received_key)
+        self.assertEqual(content, received_content)
 
 if __name__ == '__main__':
     unittest.main()
