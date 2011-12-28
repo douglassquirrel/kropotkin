d44bfdbd1eb152732c7a1f53a9e8ee4503369eb7
Douglas Squirrel <ds@douglassquirrel.com>
2011-12-28 17:28:24 +0000

    messageboard assigns a correlation_id

--- messageboard.py
+++ messageboard.py
@@ -5,7 +5,7 @@
 # This program comes with ABSOLUTELY NO WARRANTY. 
 # It is free software, and you are welcome to redistribute it under certain conditions; see the GPLv3 license in the file LICENSE for details.
 
-import datetime, json, os, pika, time
+import datetime, json, os, pika, sys, time
 
 class MessageBoard:
     def _serialise(self, c):
@@ -14,14 +14,18 @@ class MessageBoard:
     def _deserialise(self, s):
         return json.loads(s) if not (s in [None, '']) else None
 
-    def __init__(self):
+    def __init__(self, process_id=None, initial_index=0):
+        self.process_id = process_id if process_id else sys.argv[1] if 2<=len(sys.argv) else 'unknown'
+        self.correlation_index = initial_index
         connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
         self.channel = connection.channel()
         self.channel.exchange_declare(exchange='kropotkin', type='topic')
 
     def post(self, key, content=None):
-        self.channel.basic_publish(exchange='kropotkin', routing_key=key, body=self._serialise(content))
+        properties = pika.BasicProperties(correlation_id='%s.%s' % (self.process_id, self.correlation_index))
+        self.channel.basic_publish(exchange='kropotkin', routing_key=key, body=self._serialise(content), properties=properties)
         print "PID=%s %s: %s %s" % (os.getpid(), datetime.datetime.now(), key, content)
+        self.correlation_index += 1
 
     def watch_for(self, keys, queue=None):
         if not queue:
--- messageboard_test.py
+++ messageboard_test.py
@@ -24,12 +24,20 @@ class TestMessageBoard(unittest.TestCase):
         self._post_and_check_with_content(key='_test_key', content='_test_content') 
         self._post_and_check_with_content(key='_another_test_key', content='_another_test_content') 
 
+    def test_posts_with_correlation_id(self):
+        mb = messageboard.MessageBoard(314159, 0)
+        self._post_and_check_correlation_id(mb, 314159, 0)
+        self._post_and_check_correlation_id(mb, 314159, 1)
+        self._post_and_check_correlation_id(mb, 314159, 2)
+        mb = messageboard.MessageBoard(271828, 103)
+        self._post_and_check_correlation_id(mb, 271828, 103)
+
     def test_watches_for_and_gets_one_message(self):
         self._watch_for_send_and_check(key='_test_key', content={'datum': '_test_datum'})
         self._watch_for_send_and_check(key='another_test_key', content=None)
 
     def test_returns_none_if_no_message(self):
-        queue = self.mb.watch_for(key='_test_key') 
+        queue = self.mb.watch_for(keys=['_test_key']) 
         received_key, received_content = self.mb.get_one_message(queue)
         self.assertEqual(None, received_key)
         self.assertEqual(None, received_content)
@@ -48,8 +56,7 @@ class TestMessageBoard(unittest.TestCase):
 
     def test_adds_a_key_to_watch_for(self):
         keys = ['_test_key', '_test_key_two']
-        queue = self.mb.watch_for(key=keys[0]) 
-        self.mb.watch_for(key=keys[1], queue=queue)
+        queue = self.mb.watch_for(keys) 
         for key in keys:
             self.channel.basic_publish(exchange='kropotkin', routing_key=key, body=None)
 
@@ -64,8 +71,7 @@ class TestMessageBoard(unittest.TestCase):
             else:
                 received_messages.append((key, content))
 
-        queue = self.mb.watch_for(key='_test_key')
-        self.mb.watch_for(key='stop', queue=queue)
+        queue = self.mb.watch_for(keys=['_test_key', 'stop'])
         data = ['_datum1', '_datum2', '_datum3']
         for datum in data:
             self.channel.basic_publish(exchange='kropotkin', routing_key='_test_key', body=json.dumps(datum))
@@ -80,20 +86,26 @@ class TestMessageBoard(unittest.TestCase):
             if method.NAME != 'Basic.GetEmpty':
                 self.assertEqual(expected_key, method.routing_key)
                 self.assertEqual(expected_body, body)
-                return
+                return method, properties, body
             time.sleep(0.01)
         self.fail('Did not get expected message key=%s, body=%s' % (expected_key, expected_body))
 
     def _post_and_check_without_content(self, key):
         self.mb.post(key=key)
-        self._wait_for_message_and_check(expected_key=key, expected_body='')
+        return self._wait_for_message_and_check(expected_key=key, expected_body='')
 
     def _post_and_check_with_content(self, key, content):
         self.mb.post(key=key, content=content)
-        self._wait_for_message_and_check(expected_key=key, expected_body=json.dumps(content))
+        return self._wait_for_message_and_check(expected_key=key, expected_body=json.dumps(content))
+
+    def _post_and_check_correlation_id(self, mb, process_id, initial_index):
+        self.channel.queue_bind(exchange='kropotkin', queue=self.queue_name, routing_key='_test_key')
+        mb.post(key='_test_key', content='_test_content')
+        method, properties, body = self._wait_for_message_and_check(expected_key='_test_key', expected_body=json.dumps('_test_content'))
+        self.assertEqual("%s.%s" % (process_id, initial_index), properties.correlation_id)
 
     def _watch_for_send_and_check(self, key, content):
-        queue = self.mb.watch_for(key=key) 
+        queue = self.mb.watch_for(keys=[key]) 
         body = json.dumps(content) if None != content else None
         self.channel.basic_publish(exchange='kropotkin', routing_key=key, body=body)
         received_key, received_content = self.mb.get_one_message(queue)
@@ -101,7 +113,7 @@ class TestMessageBoard(unittest.TestCase):
         self.assertEqual(content, received_content)
 
     def _time_get_one_message(self, send_message, seconds_to_wait=None):
-        queue = self.mb.watch_for(key='_test_key') 
+        queue = self.mb.watch_for(keys=['_test_key']) 
         if send_message:
             self.channel.basic_publish(exchange='kropotkin', routing_key='_test_key', body=json.dumps('test_content'))
         time_before = time.time()
