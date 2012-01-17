a09c79ab659df09d4cf821535c3ce8dfb9e21df3
Douglas Squirrel <ds@douglassquirrel.com>
2012-01-17 08:09:32 +0000

    Mocks with content

--- core/mocker.py
+++ core/mocker.py
@@ -9,12 +9,17 @@ def mock(mb, message):
     if message.key == 'mock':
         message_key = str(message.content['message_key'])
         response_key = str(message.content['response_key'])
-        mocks[message_key] = response_key
+        mocks[message_key] = {'key': response_key}
+
+        if 'response_content' in message.content:
+            mocks[message_key]['content'] = str(message.content['response_content'])
+        else:
+            mocks[message_key]['content'] = None
+
         mb.watch_for(keys=[message_key], queue=queue)
         mb.post(key='ready_to_mock.%s' % message_key)
-    else:
-        if message.key in mocks:
-            mb.post(key=mocks[message.key])
+    elif message.key in mocks:
+        mb.post(key=mocks[message.key]['key'], content=mocks[message.key]['content'])
 
 mocks = {}
 mb = messageboard.MessageBoard()
--- core/mocker_test.py
+++ core/mocker_test.py
@@ -4,30 +4,33 @@
 
 import messageboard
 
-def _register_mock(message_key, response_key):
-    return mb.post_and_check(post_key='mock', post_content={'message_key': message_key, 'response_key': response_key}, 
-                             response_key='ready_to_mock.%s' % message_key)
+def _register_mock(message_key, response_key, response_content=None):
+    post_content={'message_key': message_key, 'response_key': response_key}
+    if response_content is not None:
+        post_content['response_content'] = response_content
+    return mb.post_and_check(post_key='mock', post_content=post_content, response_key='ready_to_mock.%s' % message_key)
 
-def _send_and_check(message_key, response_key):
-    return mb.post_and_check(post_key=message_key, response_key=response_key)
+def _send_and_check(message_key, response_key, response_content=None):
+    return mb.post_and_check(post_key=message_key, response_key=response_key, response_content=response_content)
 
-def mock_one_message(mb):
+def mock_one_message_without_content(mb):
     if not _register_mock(message_key='mock_test_message', response_key='mock_test_response'):
         return False
 
     return _send_and_check(message_key='mock_test_message', response_key='mock_test_response')
     
-def mock_two_messages(mb):
-    if not    _register_mock(message_key='first_mock_test_message',  response_key='first_mock_test_response') \
-       or not _register_mock(message_key='second_mock_test_message', response_key='second_mock_test_response'):
+def mock_two_messages_with_content(mb):
+    if not    _register_mock(message_key='first_mock_test_message',  response_key='first_mock_test_response', response_content='first_mock_content') \
+       or not _register_mock(message_key='second_mock_test_message', response_key='second_mock_test_response', response_content='second_mock_content'):
         return False
 
-    result = _send_and_check(message_key='first_mock_test_message', response_key='first_mock_test_response') and \
-             _send_and_check(message_key='second_mock_test_message', response_key='second_mock_test_response')
+    result = _send_and_check(message_key='first_mock_test_message', response_key='first_mock_test_response', response_content='first_mock_content') and \
+             _send_and_check(message_key='second_mock_test_message', response_key='second_mock_test_response', response_content='second_mock_content')
     return result
 
 def mocker_test(mb, message):
-    result = mock_one_message(mb) and mock_two_messages(mb)
+    result = mock_one_message_without_content(mb) and \
+             mock_two_messages_with_content(mb)
     mb.post(key='mocker_test_result', content=result)
 
 mb = messageboard.MessageBoard()
