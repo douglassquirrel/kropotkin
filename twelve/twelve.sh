0cf0eee07d87629de6a5414496beb77189ca2a4d
Douglas Squirrel <douglas.squirrel@secretsales.com>
2012-10-27 00:47:44 +0100

    Deploy hailstone simply by running tests and putting file in OUTPUT_DIR

--- twelve/twelve.sh
+++ twelve/twelve.sh
@@ -2,8 +2,10 @@
 
 SOURCE=$1
 echo "Deploying service located at $SOURCE"
-    
+
+export OUTPUT_DIR=`mktemp -d`    
 TARGET_DIR=`mktemp -d`
+
 if [[ $SOURCE = "http"* ]]; then
     echo "Downloading slug from $SOURCE"
     (cd $TARGET_DIR; wget -P $TARGET_DIR $SOURCE; tar xvf `ls .`; rm *.tar)
@@ -15,6 +17,14 @@ fi
 EXECUTABLE=`find $TARGET_DIR -maxdepth 1 -executable -type f ! -name '*~'`
 if [ -z "$EXECUTABLE" -o `echo "$EXECUTABLE" | wc -l` -ne 1 ]; then echo "Could not locate unique executable"; exit 1; fi
 EXECUTABLE=`basename $EXECUTABLE`
+
 echo "Running executable $EXECUTABLE in $TARGET_DIR"
 (cd $TARGET_DIR; ./$EXECUTABLE)
 echo "Executable $EXECUTABLE finished in $TARGET_DIR"
+
+if [ "$(ls -A $OUTPUT_DIR)" ]; then
+    echo "Serving output files from $OUTPUT_DIR on port $PORT"
+    (cd $OUTPUT_DIR; python -m SimpleHTTPServer $PORT)
+else
+    echo "No output, ending deployment"
+fi
