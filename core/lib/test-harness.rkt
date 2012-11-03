58a8fb4be873206dfdc38f4b7022830bb11aa3e7
Douglas Squirrel <douglas.squirrel@secretsales.com>
2012-11-03 00:45:25 +0000

    functional tests for test harness

--- core/lib/test-harness.rkt
+++ core/lib/test-harness.rkt
@@ -1,4 +1,9 @@
 #lang racket
+(require rackunit rackunit/text-ui)
 (provide execute-tests)
 
-(define (execute-tests) #f)
+(define (execute-tests tests)
+  (cond ((empty? tests) #t)
+	(else
+	 (let ((number-of-failures (run-tests tests)))
+	   (= 0 number-of-failures)))))
