6d7afd2bf422f53907b5754f8233237b2f323fa6
Douglas Squirrel <douglas.squirrel@secretsales.com>
2012-11-15 21:34:01 +0000

    clean up catalog tests

--- twelve/publisher/catalog/catalog-tests.rkt
+++ twelve/publisher/catalog/catalog-tests.rkt
@@ -2,25 +2,22 @@
 (require rackunit rackunit/text-ui "catalog.rkt")
 (provide execute-catalog-tests)
 
-(define tests (test-suite "Publisher catalog tests"
-			  (test-case "add" (check-not-exn (lambda () (add-to-catalog #:name "stations"
-										     #:creation-datetime "2012-01-01 00:00:00"
-										     #:contents #"Ashford, Westenghanger, Sandling"))))
-			  (test-case "add and retrieve one item" 
-				     (add-to-catalog #:name "colours"
-						     #:creation-datetime "2012-02-02 00:00:00"
-						     #:contents #"red, black")
-				     (check-equal? (get-latest-with-name "colours") #"red, black"))
-			  (test-case "add and retrieve two items" 
-				     (add-to-catalog #:name "colours"
-						     #:creation-datetime "2012-02-03 00:00:11"
-						     #:contents #"red, black")
-				     (add-to-catalog #:name "dogs"
-						     #:creation-datetime "2012-02-03 00:02:22"
-						     #:contents #"Labrador, poodle")
-				     (check-equal? (get-latest-with-name "colours") #"red, black")
-				     (check-equal? (get-latest-with-name "dogs")    #"Labrador, poodle"))))
+(random-seed 314159265)
+(define (add-test-data-to-catalog name [creation-datetime "2012-01-01 00:00:00"])
+  (let ((random-data (format "~a" (random 1000000))))
+    (add-to-catalog #:name name #:creation-datetime creation-datetime #:contents random-data)
+    random-data))
 
+(define tests (test-suite "Publisher catalog tests"
+			  (test-case "add" (check-not-exn (lambda () (add-test-data-to-catalog "test-add"))))
+			  (test-case "add and retrieve one item" 
+				     (let ((data (add-test-data-to-catalog "test-one-add")))
+				       (check-equal? (get-latest-with-name "test-one-add") data)))
+			  (test-case "add and retrieve two items" 
+				     (let ((data-1 (add-test-data-to-catalog "test-two-add-1"))
+				           (data-2 (add-test-data-to-catalog "test-two-add-2")))
+				       (check-equal? (get-latest-with-name "test-two-add-1") data-1)
+				       (check-equal? (get-latest-with-name "test-two-add-2") data-2)))))
 
 (define (execute-catalog-tests)
   (let ((number-of-failures (run-tests tests 'verbose)))
