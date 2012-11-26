a51ac9921d8edcff759048ba9a24bfad00901bc2
Douglas Squirrel <douglas.squirrel@secretsales.com>
2012-11-26 09:56:49 +0000

    catalog does not depend on CATALOG_FILE env var

--- twelve/publisher/catalog/catalog-tests.rkt
+++ twelve/publisher/catalog/catalog-tests.rkt
@@ -2,28 +2,31 @@
 (require rackunit rackunit/text-ui "catalog.rkt")
 (provide execute-catalog-tests)
 
+(define CATALOG_FILE (getenv "CATALOG_FILE"))
+(define cat (make-catalog CATALOG_FILE))
+
 (random-seed 314159265)
 (define (add-test-data-to-catalog name [creation-datetime "2012-01-01 00:00:00"])
   (let ((random-data (string->bytes/utf-8 (format "~a" (random 1000000)))))
-    (add-to-catalog #:name name #:creation-datetime creation-datetime #:contents random-data)
+    (add-to-catalog #:catalog cat #:name name #:creation-datetime creation-datetime #:contents random-data)
     random-data))
 
 (define tests (test-suite "Publisher catalog tests"
 			  (test-case "add" (check-not-exn (lambda () (add-test-data-to-catalog "test-add"))))
 			  (test-case "add and retrieve one item" 
 				     (let ((data (add-test-data-to-catalog "test-one-add")))
-				       (check-equal? (get-latest-with-name "test-one-add") data)))
+				       (check-equal? (get-latest-with-name cat "test-one-add") data)))
 			  (test-case "add and retrieve two items" 
 				     (let ((data-1 (add-test-data-to-catalog "test-two-add-1"))
 				           (data-2 (add-test-data-to-catalog "test-two-add-2")))
-				       (check-equal? (get-latest-with-name "test-two-add-1") data-1)
-				       (check-equal? (get-latest-with-name "test-two-add-2") data-2)))
+				       (check-equal? (get-latest-with-name cat "test-two-add-1") data-1)
+				       (check-equal? (get-latest-with-name cat "test-two-add-2") data-2)))
 			  (test-case "add multiple items with same name in any order, always retrieve latest"
 				     (add-test-data-to-catalog "test-multiple-same" "2012-01-01 01:01:01")
 				     (let ((most-recent-data (add-test-data-to-catalog "test-multiple-same" "2012-02-02 02:02:02")))
-				       (check-equal? (get-latest-with-name "test-multiple-same") most-recent-data)
+				       (check-equal? (get-latest-with-name cat "test-multiple-same") most-recent-data)
 				       (add-test-data-to-catalog "test-multiple-same" "2012-01-03 03:03:03")
-				       (check-equal? (get-latest-with-name "test-multiple-same") most-recent-data)))))
+				       (check-equal? (get-latest-with-name cat "test-multiple-same") most-recent-data)))))
 
 (define (execute-catalog-tests)
   (let ((number-of-failures (run-tests tests 'verbose)))
--- twelve/publisher/catalog/catalog.rkt
+++ twelve/publisher/catalog/catalog.rkt
@@ -2,27 +2,30 @@
 (require "vendor/planet/jaymccarthy/sqlite.rkt")
 (define DATETIME_REGEX #px"\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}")
 
+(define (catalog? cat) db?)
+
 (provide/contract
- (add-to-catalog (#:name string? #:creation-datetime (and/c string? DATETIME_REGEX) #:contents bytes? . -> . void?))
- (get-latest-with-name (string? . -> . (or/c bytes? #f))))
+ (catalog? (any/c . -> . boolean?))
+ (make-catalog (string? . -> . catalog?))
+ (add-to-catalog (#:catalog catalog? #:name string? #:creation-datetime (and/c string? DATETIME_REGEX) #:contents bytes? . -> . void?))
+ (get-latest-with-name (catalog? string? . -> . (or/c bytes? #f))))
 
 (define CREATE-SQL   "create table if not exists catalog(name text, creation_datetime text, contents blob)")
 (define STORE-SQL    "insert into catalog (name, creation_datetime, contents) values (?, ?, ?)")
 (define RETRIEVE-SQL "select contents from catalog where name = ? order by creation_datetime desc")
 
-(define CATALOG_FILE (getenv "CATALOG_FILE"))
-(printf "Storing catalog in ~a\n" CATALOG_FILE)
+(define (make-catalog catalog-file)
+  (let ((cat (open (string->path catalog-file))))
+    (exec/ignore cat CREATE-SQL)
+    cat))
 
-(define db (open (string->path CATALOG_FILE)))
-(exec/ignore db CREATE-SQL)
-
-(define (add-to-catalog #:name name #:creation-datetime creation-datetime #:contents contents)
-  (let ((store-statement (prepare db STORE-SQL)))
+(define (add-to-catalog #:catalog cat #:name name #:creation-datetime creation-datetime #:contents contents)
+  (let ((store-statement (prepare cat STORE-SQL)))
     (load-params store-statement name creation-datetime contents)
     (run store-statement)))
   
-(define (get-latest-with-name name) 
-  (let ((retrieve-statement (prepare db RETRIEVE-SQL)))
+(define (get-latest-with-name cat name) 
+  (let ((retrieve-statement (prepare cat RETRIEVE-SQL)))
     (load-params retrieve-statement name)
     (let ((result (step retrieve-statement)))
       (cond ((and (vector? result) (> (vector-length result) 0)) (vector-ref result 0))
