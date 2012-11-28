ee20219449154af8f77af4424207ddff42aabb99
Douglas Squirrel <douglas.squirrel@secretsales.com>
2012-11-28 14:44:54 +0000

    secretary passes file to catalog

--- twelve/publisher/secretary/secretary.rkt
+++ twelve/publisher/secretary/secretary.rkt
@@ -9,16 +9,19 @@
    (parameterize ((date-display-format 'iso-8601)) 
 		 (date->string (seconds->date (file-or-directory-modify-seconds file)) #t)))
 
-(define (add-file-to-catalog filename)
+(define (add-file-to-catalog cat filename)
   (let ((file (build-path INPUT_DIR filename)))
     (printf "Adding ~a to catalog\n" file)
-    (add-to-catalog #:name              (path->string filename) 
+    (add-to-catalog #:catalog           cat 
+		    #:name              (path->string filename) 
 		    #:creation-datetime (creation-datetime file)
 		    #:contents          (file->bytes file))
     (delete-file file)))
 
+(define cat (make-catalog (getenv "CATALOG_FILE")))
+
 (define (run-secretary)
   (displayln "Secretary running")
-  (for-each add-file-to-catalog (directory-list INPUT_DIR))
+  (for-each ((curry add-file-to-catalog) cat) (directory-list INPUT_DIR))
   (sleep 1)
   (run-secretary))
