813fa5c2d4a554034d37d4d17d9a78d712864710
Douglas Squirrel <douglas.squirrel@secretsales.com>
2013-04-09 20:54:33 +0100

    First go at python module

---
+++ module/kropotkin.py
@@ -0,0 +1,3 @@
+def start_factspace(name, port, components_directory):
+    print "Starting factspace %s on port %d using %s"\
+        % (name, port, components_directory)
---
+++ module/setup.py
@@ -0,0 +1,4 @@
+from distutils.core import setup
+setup(name='kropotkin',
+      version='0.0.1',
+      py_modules=['kropotkin'])
