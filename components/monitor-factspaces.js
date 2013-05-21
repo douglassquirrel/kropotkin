29a9203a40df91c1ff1a5db3444b920087a453b1
Douglas Squirrel <douglas.squirrel@secretsales.com>
2013-05-21 07:39:31 +0100

    Factspace list in monitor page

---
+++ components/monitor-factspaces.js
@@ -0,0 +1,23 @@
+function check_factspaces() {
+    get_all_facts('kropotkin', 'factspace', [], display_factspaces);
+}
+
+function display_factspaces(facts) {
+    var factspace_select = document.querySelectorAll('*[data-factspace]')[0];
+    var options = factspace_select.options;
+    var option_values = []
+    for (i=0; i<options.length; i++) {
+        option_values.push(options[i].value)
+    }
+    for (i=0; i<facts.length; i++) {
+        var name = facts[i]['name'];
+        if (option_values.indexOf(name) < 0) {
+            console.log("Adding " + name);
+            var option=document.createElement("option");
+            option.text=name;
+            options.add(option, null);
+        }
+    }
+}
+
+setInterval(check_factspaces, 1000);
