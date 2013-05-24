96f665caf47748a1c9d5e44892b6f5dd3b3f3d15
Douglas Squirrel <douglas.squirrel@secretsales.com>
2013-05-24 21:58:31 +0100

    radio buttons for constitution table

--- components/monitor-constitution.js
+++ components/monitor-constitution.js
@@ -4,17 +4,30 @@ function check_constitution() {
     get_all_facts(factspace, 'constitution_element', {}, display_constitution);
 }
 
+var RADIO_HTML = '<input type="radio" name="const_inspect" ';
+RADIO_HTML    +=        'value="VALUE" CHECKED/>';
+
+function get_see_statements_element() {
+    var radio_buttons = document.getElementsByName('const_inspect');
+    for (var i=0; i<radio_buttons.length; i++) {
+        if (radio_buttons[i].checked) return radio_buttons[i].value;
+    }
+    return null;
+}
+
 function display_constitution(facts) {
+    var selected_element = get_see_statements_element();
     var const_table = document.querySelectorAll('*[data-constitution]')[0];
     var initial_table_rows = const_table.rows.length;
-    for (i=0; i < initial_table_rows - facts.length; i++) {
+    for (var i=0; i < initial_table_rows - facts.length; i++) {
         const_table.deleteRow(-1);
     }
-    for (i=0; i < facts.length - initial_table_rows; i++) {
+    for (var i=0; i < facts.length - initial_table_rows; i++) {
         row = const_table.insertRow(-1);
         row.insertCell(0);
         row.insertCell(1);
         row.insertCell(2);
+        row.insertCell(3);
     }
     facts.sort(function(f, g) {
         var a = f['type'];
@@ -23,11 +36,20 @@ function display_constitution(facts) {
         if (a>b) return 1;
         return 0;
     });
-    for (i=0; i < facts.length; i++) {
+    for (var i=0; i < facts.length; i++) {
         row = const_table.rows[i];
         row.cells[0].innerHTML = facts[i]['type'];
         row.cells[1].innerHTML = facts[i]['keys'].join();
         row.cells[2].innerHTML = facts[i]['translation'];
+
+        if (selected_element == facts[i]['type']) {
+            checked = 'checked="checked"';
+        } else {
+            checked = '';
+        }
+        html = RADIO_HTML.replace('VALUE', facts[i]['type']);
+        html = html.replace('CHECKED', checked);
+        row.cells[3].innerHTML = html;
     }
 }
 
